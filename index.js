55600882e1d54b47ee2414494aa7799316ded194
Meltrust <original.mtapia@outlook.com>
2021-12-23 16:38:00 -0600

    Fix hide menu function

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-// init
-
+/* eslint-disable no-unused-vars */
+const elem = document.getElementById('menuMobile');
 const toggleMobileMenu = () => {
-  const elem = document.getElementById('menuMobile');
-  if (elem.style.display === 'block') {
-    elem.style.display = 'none';
+  // const logoName = document.getElementById('logo');
+  // const menuButton = document.getElementById('menuBars');
+  if (!elem.classList.contains('show-menu-mobile')) {
+    elem.classList.add('show-menu-mobile');
   } else {
-    elem.style.display = 'block';
+    elem.classList.remove('show-menu-mobile');
   }
-}
+  // if (elem.style.display === 'block') {
+  //   elem.style.display = 'none';
+  // logoName.style.display = 'none';
+  // menuButton.style.display = 'none';
+  // } else {
+  // elem.style.display = 'block';
+  // menuButton.style.display = 'block';
+  // logoName.style.display = 'block';
+};
+
+const item = document.querySelectorAll('.list-menu-item');
+
+// const closeMobileMenu = () => {
+//   item.forEach((e) => e.classList.remove('show-menu-mobile'));
+// };
+item.forEach((e) => {
+  e.addEventListener('click', (e) => elem.classList.remove('show-menu-mobile'));
+});
