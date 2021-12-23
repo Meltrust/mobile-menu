a83ae55d2b2fbfe41d678e339fe07d0d6aea8f2a
Meltrust <original.mtapia@outlook.com>
2021-12-23 16:40:33 -0600

    fix mobile menu

--- index.js
+++ index.js
@@ -8,21 +8,8 @@ const toggleMobileMenu = () => {
   } else {
     elem.classList.remove('show-menu-mobile');
   }
-  // if (elem.style.display === 'block') {
-  //   elem.style.display = 'none';
-  // logoName.style.display = 'none';
-  // menuButton.style.display = 'none';
-  // } else {
-  // elem.style.display = 'block';
-  // menuButton.style.display = 'block';
-  // logoName.style.display = 'block';
 };
-
 const item = document.querySelectorAll('.list-menu-item');
-
-// const closeMobileMenu = () => {
-//   item.forEach((e) => e.classList.remove('show-menu-mobile'));
-// };
 item.forEach((e) => {
   e.addEventListener('click', (e) => elem.classList.remove('show-menu-mobile'));
 });
