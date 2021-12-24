717c9ba64569f26d442ebfbf0d475685ee6c99ce
Meltrust <original.mtapia@outlook.com>
2021-12-24 07:21:25 -0600

    Delete commented code, style mobile menu, align js variables

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const elem = document.getElementById('menuMobile');
 const item = document.querySelectorAll('.list-menu-item');
+const logo = document.getElementById('logo');
+const menubars = document.getElementById('menuBars');
 
 const toggleMobileMenu = () => {
   if (!elem.classList.contains('show-menu-mobile')) {
     elem.classList.add('show-menu-mobile');
+    logo.style.display = 'none';
+    menubars.style.display = 'none';
   } else {
     elem.classList.remove('show-menu-mobile');
+    logo.style.display = 'block';
+    menubars.style.display = 'inline-block';
   }
 };
 
 const button = document.getElementById('menuBars');
 button.addEventListener('click', toggleMobileMenu);
-
+console.log(elem);
 item.forEach((e) => {
-  e.addEventListener('click', () => elem.classList.remove('show-menu-mobile'));
+  e.addEventListener('click', toggleMobileMenu);
 });
