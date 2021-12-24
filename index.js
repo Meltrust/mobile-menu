9652e410040b3e095d0fad244cec4dd446814a89
Meltrust <original.mtapia@outlook.com>
2021-12-24 06:10:44 -0600

    Remove commented code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const elem = document.getElementById('menuMobile');
+const item = document.querySelectorAll('.list-menu-item');
+
 const toggleMobileMenu = () => {
-  // const logoName = document.getElementById('logo');
-  // const menuButton = document.getElementById('menuBars');
   if (!elem.classList.contains('show-menu-mobile')) {
     elem.classList.add('show-menu-mobile');
   } else {
@@ -12,7 +12,6 @@ const toggleMobileMenu = () => {
 const button = document.getElementById('menuBars');
 button.addEventListener('click', toggleMobileMenu);
 
-const item = document.querySelectorAll('.list-menu-item');
 item.forEach((e) => {
   e.addEventListener('click', () => elem.classList.remove('show-menu-mobile'));
 });
